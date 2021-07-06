f8d65e1a7c077bd41eaf786aac43742472cb233d
defzsk <425172913@qq.com>
2021-07-06 11:03:02 +0800

    完成项目初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    home
+    <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
